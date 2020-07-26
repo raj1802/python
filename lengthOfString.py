65e785fec60ff303df882c3880f9d46d40f98285
raj1802 <dheeraj18200@gmail.com>
2020-07-26 13:03:14 +0530

    Initial commit

---
+++ lengthOfString.py
@@ -0,0 +1,13 @@
+def findLen(str): 
+    counter = 0
+    while str[counter:]:
+        print(str[counter:])
+        counter += 1
+        
+    return counter 
+  
+str = "geeks"
+print(findLen(str))
+while(str):
+	print(5)
+
